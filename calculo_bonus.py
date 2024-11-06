e2367485bcf0cc2318da7c76b22eb080f2f5cfa1
Octalink <michael.defaveri@octalink.com.br>
2024-11-05 23:58:11 -0300

    Inclusão do calculo de bonus do kpi

---
+++ calculo_bonus.py
@@ -0,0 +1,8 @@
+# Criando calculo do kpi calculo bonus
+
+nome = input("Digite seu nome: ")
+salario = float(input("Digite seu salário: "))
+bonus = float(input("Digite o valor do bônus recebido: "))
+result = (1000 + salario) * bonus
+
+print( nome,", o valor total do bônus a ser recebido é de: ",result)

08655c35e900351230be38131cdc5885f0ec12d1
Octalink <michael.defaveri@octalink.com.br>
2024-11-05 23:33:48 -0300

    Adicionando a soma dos valores imputados em python

---
+++ programa02.py
@@ -0,0 +1,15 @@
+# Crie um programa com duas entradas de numeros e some eles
+
+# Take input values from the user
+valor01 = input("Digite um valor: ")
+valor02 = input("Digite outro valor: ")
+
+# Convert the strings to floats
+valor01 = float(valor01)
+valor02 = float(valor02)
+
+# Calculate the sum
+soma = valor01 + valor02
+
+# Print the result
+print("A soma dos valores é:", soma)
